f3e27fd55e9fe9566db042481b98e61ef1969512
Sathish <sathish.jegatheeswaran@smile-direct.ch>
2023-01-22 19:17:09 +0100

    test

--- src/test/java/com/example/helloworld/controller/HelloWorldControllerTest.java
+++ src/test/java/com/example/helloworld/controller/HelloWorldControllerTest.java
@@ -22,6 +22,6 @@ public class HelloWorldControllerTest {
 
         mockMvc.perform(get("/hello"))
                 .andExpect(status().isOk())
-                .andExpect(content().string("Hello, Smile CH!"));
+                .andExpect(content().string("Hello, Smile!"));
     }
 }
