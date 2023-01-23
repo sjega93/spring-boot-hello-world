bd9bb75b60f7c3b2f25f7d48bbc0339544febd89
Sathish <sathish.jegatheeswaran@smile-direct.ch>
2023-01-23 14:41:54 +0100

    test

--- src/main/java/com/example/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/example/helloworld/controller/HelloWorldController.java
@@ -8,6 +8,6 @@ public class HelloWorldController {
 
     @GetMapping("/hello")
     public String sendGreetings() {
-        return "Hello, Smiletest3!";
+        return "Hello, Smiletest4!";
     }
 }
